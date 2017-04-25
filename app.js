4831ff461fe042ae431779bb94ac10ccaffc9eb1
Julio Escobar <julioescobar900@gmail.com>
2017-04-25 17:29:04 -0300

    Add user software

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var useragent = require('express-useragent');
 
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(useragent.express());
+
 
 app.set('port', (process.env.PORT || 3000));
 
 
 
-app.get('/api/whoami', function(req, res) {
+app.get('/userinfo', function(req, res) {
     var ipU = req.ip;
     var languageU = req.acceptsLanguages()[0];
+    var softwareU = req.useragent.os;
     
     res.json({ipadress: ipU, language: languageU, software: softwareU});
 });
